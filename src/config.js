236eead308d50b963bb17ed5275573a905e3d45c
Zack <altschulerzack@yahoo.com>
2020-10-23 19:10:47 -0700

    production

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://zackaltschuler@localhost/Boozy",
-  TEST_DB_URL:
-    process.env.TEST_DB_URL ||
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://zackaltschuler@localhost/Boozy",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
     "postgresql://zackaltschuler@localhost/Boozy-test",
 };
