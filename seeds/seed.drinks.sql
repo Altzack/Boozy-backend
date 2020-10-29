03e8b6229ed6e389300edaf1e02f61e6b3e7169f
Zack <altschulerzack@yahoo.com>
2020-10-28 17:17:13 -0700

    production

--- seeds/seed.drinks.sql
+++ seeds/seed.drinks.sql
@@ -2,13 +2,26 @@ INSERT INTO drinks (
         title,
         alcohol,
         mixers,
+        liqueurs,
         juices,
+        other,
         instructions
     )
 VALUES (
         'Zacks special potion',
         'Vodka',
         'Sprite',
+        'none',
         'Cranberry juice',
-        'Mix 10 shots of vodka with a splash of sprite and cranberry juice. Enjoy'
+        'none',
+        'Mix 2 shots of vodka with a splash of sprite and cranberry juice. Enjoy'
+    ),
+    (
+        'Harrows strong soda',
+        'Rum',
+        'coke',
+        'none',
+        'none',
+        'none',
+        'Mix 3 shots of rum with a good pour of coke. Bottoms up!'
     )
